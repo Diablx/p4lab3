c5e1c930e836457ec3820873b6008ff508ff28c7
Diablx <kajetanboruta@gmail.com>
2020-03-07 17:20:42 +0100

    First commit

---
+++ ConsoleApplication2/DB.cs
@@ -0,0 +1,47 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public class DB
+    {
+        public void Select(IDbConnection conn)
+        {
+            var sql = "SELECT * FROM Northwind.dbo.Region";
+            var result = conn.Query<Region>(sql);
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.RegionID.ToString()}: {item.RegionDescription}");
+            }
+            
+        }
+
+        public void Insert(IDbConnection connection)
+        {
+            var sql = $"INSERT INTO ?() VALUES()";
+            var result = connection.Query<Region>(sql);
+        }
+
+        public void Update(IDbConnection connection)
+        {
+            var sql = $"UPDATE ? SET ? = ? WHERE ? = ?";
+            var result = connection.Query<Region>(sql);
+
+        }
+
+        public void Delete(IDbConnection connection)
+        {
+            var sql = $"DELETE ? FROM ? WHERE ? = ?";
+            var result = connection.Query<Region>(sql);
+
+        }
+
+
+    }
+}
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connstr = @"Data Source = (localdb)\MSSQLLocalDB; Initial Catalog = master; Integrated Security = True; Connect Timeout = 30; Encrypt = False; TrustServerCertificate = True; ApplicationIntent = ReadWrite; MultiSubnetFailover = False";
+            var connection = new SqlConnection(connstr);
+            connection.Open();
+            var db = new DB();
+            db.Select(connection);
+            connection.Close();
+        }
+    }
+}
